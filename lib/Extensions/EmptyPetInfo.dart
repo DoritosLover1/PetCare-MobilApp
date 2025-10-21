0fc4bdaade090fb3dcc477d06871914df82120ac
DoritosLover1 <ihsandmrci10@gmail.com>
2025-10-21 14:45:30 +0300

    Changed because of Testing error

--- lib/Extensions/EmptyPetInfo.dart
+++ lib/Extensions/EmptyPetInfo.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class EmptyInfoCard extends StatelessWidget{
@@ -8,7 +7,6 @@ class EmptyInfoCard extends StatelessWidget{
   final String sectionText;
   final String subText;
   final IconData icon;
-
 
   const EmptyInfoCard({
     super.key,
@@ -22,57 +20,62 @@ class EmptyInfoCard extends StatelessWidget{
   @override
   Widget build(BuildContext context){
     return Container(
-          decoration:  BoxDecoration(
-            color: Colors.white,
-            borderRadius: BorderRadius.circular(20),
-            boxShadow: [
-              BoxShadow(
-                color: const Color.fromARGB(115, 104, 104, 104),
-                spreadRadius: 2,
-                blurRadius: 10,
-              )
-            ]
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(20),
+        boxShadow: [
+          BoxShadow(
+            color: const Color.fromARGB(115, 104, 104, 104),
+            spreadRadius: 2,
+            blurRadius: 10,
+          )
+        ]
+      ),
+      padding: EdgeInsets.all(screenWidth * 0.03),
+      width: screenWidth * 0.6,
+      height: screenHeight * 0.5,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min, // Add this line
+        children: [
+          Container(
+            width: screenWidth * 0.2,
+            height: screenWidth * 0.2,
+            decoration: BoxDecoration(
+              color: const Color.fromARGB(255, 200, 238, 215),
+              shape: BoxShape.circle
+            ),
+            child: Icon(
+              icon,
+              size: screenWidth * 0.1,
+              color: const Color(0xFF20DF6C),
+            ),
           ),
-          padding: EdgeInsets.all(screenWidth * 0.03),
-          width: screenWidth * 0.6,
-          height: screenHeight * 0.5,
-          child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              width: screenWidth * 0.2,
-              height: screenWidth * 0.2,
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 200, 238, 215),
-                shape: BoxShape.circle
-              ),
-              child: Icon(
-                icon,
-                size: screenWidth* 0.1,
-                color: const Color(0xFF20DF6C),
-              ),
+          SizedBox(height: screenHeight * 0.015),
+          Text(
+            sectionText, 
+            style: TextStyle(
+              fontSize: screenHeight * 0.025, 
+              fontWeight: FontWeight.bold
             ),
-            SizedBox(height: screenHeight * 0.015),
-            Text(
-              sectionText, 
-              style: TextStyle(
-                fontSize: screenHeight * 0.025, 
-                fontWeight: FontWeight.bold
-              ),
-              textAlign: TextAlign.center,
+            textAlign: TextAlign.center,
+            maxLines: 2, // Add this
+            overflow: TextOverflow.ellipsis, // Add this
+          ),
+          SizedBox(height: screenHeight * 0.01),
+          Text(
+            subText, 
+            style: TextStyle(
+              fontSize: screenHeight * 0.015, 
+              fontWeight: FontWeight.bold,
+              color: const Color.fromARGB(255, 122, 122, 122)
             ),
-            SizedBox(height: screenHeight * 0.01),
-            Text(
-              subText, 
-              style: TextStyle(
-                fontSize: screenHeight * 0.015, 
-                fontWeight: FontWeight.bold,
-                color: const Color.fromARGB(255, 122, 122, 122)
-              ),
-              textAlign: TextAlign.center,
-            ),
-          ],
-     ),
+            textAlign: TextAlign.center,
+            maxLines: 2, // Add this
+            overflow: TextOverflow.ellipsis, // Add this
+          ),
+        ],
+      ),
     );
   }
 }
